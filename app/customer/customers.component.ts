0d769123eb9fa0e2b69d0da8fa82bfee72ee0a3e
Alencar <Douglas_Alencar@dell.com>
2016-07-14 11:51:52 +0100

    no async

--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -12,15 +12,28 @@ import { CustomerService } from './customer.service';
     providers: [CustomerService]
 })
 export class CustomersComponent implements OnInit {
-    customers: Observable<any[]>;
+    customers: any[];
 
     constructor(private _customerService: CustomerService) { }
 
-    ngOnInit() { 
-        this.customers = this._customerService.getCustomers()
+    ngOnInit() {
+        this._customerService.getCustomers()
+            .then((customers) => this.customers = customers)
             .catch((err) => {
                 console.log(err);
-                return Observable.of(true);
             });
+
+        // Promise<any[]> 
+        // this.customers = this._customerService.getCustomers()
+        //     .catch((err) => {
+        //         console.log(err);
+        //     });
+
+        //  Rx Observable version
+        //  this.customers = this._customerService.getCustomers()
+        //     .catch((err) => {
+        //         console.log(err);
+        //         return Observable.of(true);
+        //     });            
     }
 }
