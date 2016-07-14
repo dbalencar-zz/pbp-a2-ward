d1905b680b4a05b666329a793397338187ebdacb
Alencar <Douglas_Alencar@dell.com>
2016-07-14 12:00:10 +0100

    4 ways to get data

--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -17,11 +17,20 @@ export class CustomersComponent implements OnInit {
     constructor(private _customerService: CustomerService) { }
 
     ngOnInit() {
-        this._customerService.getCustomers()
-            .then((customers) => this.customers = customers)
-            .catch((err) => {
-                console.log(err);
-            });
+
+         //Rx Observable version
+         this._customerService.getCustomers_RxObservable()
+            .subscribe(
+                (customers) => this.customers = customers,
+                (err) => { console.log(err); }
+            ); 
+
+        // Promise to array
+        // this._customerService.getCustomers()
+        //     .then((customers) => this.customers = customers)
+        //     .catch((err) => {
+        //         console.log(err);
+        //     });
 
         // Promise<any[]> 
         // this.customers = this._customerService.getCustomers()
@@ -30,7 +39,7 @@ export class CustomersComponent implements OnInit {
         //     });
 
         //  Rx Observable version
-        //  this.customers = this._customerService.getCustomers()
+        //  this.customers = this._customerService.getCustomers_RxObservable()
         //     .catch((err) => {
         //         console.log(err);
         //         return Observable.of(true);
